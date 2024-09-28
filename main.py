dd8c151bd1120682251283192f01d13703ddf48d
MinhTamNT <minhtam78945@gmail.com>
2024-09-28 10:07:54 +0700

    chore : comment code and fix

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import pandas as pd
-from stock_monitor import fetch_stock_data, calculate_ema, generate_signal, convert_resolution_to_seconds
 import time
 from datetime import datetime
+
+import pandas as pd
+from colorama import Fore
+
+from stock_monitor import fetch_stock_data, calculate_ema, generate_signal
+
 
 def run_stock_monitoring(symbols, start_date, end_date, resolution):
     dataframes = {symbol: pd.DataFrame(columns=['time', 'price']) for symbol in symbols}
@@ -9,7 +13,6 @@ def run_stock_monitoring(symbols, start_date, end_date, resolution):
     while True:
         current_time = datetime.now()
 
-        # Define trading time
         start_trading_time = current_time.replace(hour=9, minute=15, second=0, microsecond=0).time()
         end_trading_time = current_time.replace(hour=15, minute=0, second=0, microsecond=0).time()
 
@@ -23,28 +26,73 @@ def run_stock_monitoring(symbols, start_date, end_date, resolution):
                     else:
                         dataframes[symbol] = df
 
-                    # Only calculate EMA if there are enough data points
                     if len(dataframes[symbol]) >= 5:
                         ema_df = calculate_ema(dataframes[symbol])
                         signal = generate_signal(symbol, ema_df)
-                        print(f"\nMã cổ phiếu: {symbol}")
-                        print(f"Tín hiệu: {signal}")
-                        print(f"Giá hiện tại: {ema_df.iloc[-1]['price']:.2f}, EMA_5: {ema_df.iloc[-1]['EMA_5']:.2f}")
-                        print(f"===========================================================\n")
+
+                        current_price = ema_df.iloc[-1]['price']
+                        ema_5 = ema_df.iloc[-1]['EMA_5']
+
+                        formatted_current_price = f"{current_price:,.0f}".replace(',', '.').replace('.', ',', 1)
+                        formatted_ema_5 = f"{ema_5:,.0f}".replace(',', '.').replace('.', ',', 1)
+
+                        if signal == 'Mua':
+                            signal_color = Fore.GREEN
+                        elif signal == 'Bán':
+                            signal_color = Fore.RED
+                        else:
+                            signal_color = Fore.WHITE
+
+                        print(f"Mã CK:  {symbol}")
+                        print(f"Tín hiệu: {signal_color}{signal}{Fore.RESET}")
+                        print(f"Giá hiện tại: {formatted_current_price}")
+                        print(f"EMA_5: {formatted_ema_5}")
+                        print("===========================================================\n")
+
                     else:
                         print(f"Không đủ dữ liệu cho {symbol} để tính toán EMA. Cần 5 điểm dữ liệu, hiện có {len(dataframes[symbol])}.")
                 else:
                     print(f"Không lấy được dữ liệu cho mã {symbol}")
 
-            print(f"\nChờ {resolution} phút...\n")
-            time.sleep(convert_resolution_to_seconds(resolution))
+            if resolution.isdigit():
+                minutes = int(resolution)
+                print(f"\nChờ {minutes} phút...\n")
+                time.sleep(minutes * 60)
+            elif resolution.upper() == "D":
+                print("\nChờ 1 ngày...\n")
+                time.sleep(24 * 60 * 60)
+            elif resolution.upper() == "M":
+                print("\nChờ 1 tháng...\n")
+                time.sleep(30 * 24 * 60 * 60)
+            else:
+                print("\nĐộ phân giải không hợp lệ.\n")
+                break
+
         else:
             print(f"Hiện tại là {current_time.strftime('%H:%M:%S')}, ngoài giờ giao dịch (9:15 AM - 3:00 PM). Chờ đến giờ...\n")
             time.sleep(60)
 
 if __name__ == "__main__":
-    start_date = "2024-09-27"
-    end_date = "2024-09-27"
+    """
+     Tham số của resolution :
+        - 1 : 1 phút
+        - 5 : 5 phút
+        - 15 : 15 phút
+        - 60 : 1 gi
+        - D : 1 ngày
+        - M : 1 Tháng
+        - w : 1 ngày
+    - lưu ý tham số của resolution theo tiêu chuẩn định nghĩa sẵn của API của TBCS nên dùng những tham số trên nhá.
+    """
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    start_date = current_date
+
+    end_date = current_date
+
     symbols = ["VN30F2410", "FPT"]
+
+    # Can be "1", "5", "15", "60", "D", or "M"
     resolution = "1"
+
     run_stock_monitoring(symbols, start_date, end_date, resolution)
--- stock_monitor.py
+++ stock_monitor.py
@@ -1,10 +1,8 @@
 import requests
 import pandas as pd
-import time
-from datetime import datetime, timedelta
+from datetime import datetime
 from config import *
 
-# Cooldown duration in seconds
 last_signals = {}
 
 def send_notify_signal(symbol, signal_date, signal_type, current_price, recommended_price):
@@ -16,11 +14,12 @@ def send_notify_signal(symbol, signal_date, signal_type, current_price, recommen
         'current_price': recommended_price
     }
 
-    url = f"{API}?username={USERNAME}"  # Fixed URL format
+    url = f"{API}?username={USERNAME}"
     try:
         response = requests.post(url, json=payload)
         if response.status_code == 200:
-            print(f"Tín hiệu {signal_type} cho mã {symbol} đã được gửi thành công.")
+            # print(f"Tín hiệu {signal_type} cho mã {symbol} đã được gửi thành công.")
+            pass
         else:
             print(f"API thất bại với mã trạng thái: {response.status_code}")
     except requests.exceptions.RequestException as e:
@@ -33,7 +32,6 @@ def fetch_stock_data(symbol, start_date, end_date, resolution="1"):
     if symbol.startswith("VN30"):
         url = f"{API_GET_PRICE_VN30}?resolution={resolution}&ticker={symbol}&type=derivative&start={start}&to={end}&countBack=5"
         response = requests.get(url)
-        print(f"Response Body: {response.json()}")
         if response.status_code == 200:
             data = response.json()
             if data and 'data' in data:
@@ -82,33 +80,28 @@ def generate_signal(symbol, ema_df):
     current_price = ema_df.iloc[-1]['price']
     recommended_price = ema_df.iloc[-1]['EMA_5']
     signal_date = datetime.now().isoformat()
-    now = datetime.now()
-
-    # Initialize last signal if not present
     if symbol not in last_signals:
         last_signals[symbol] = {'type': None, 'time': None}
 
-    # last_signal_type = last_signals[symbol]['type']
-    # last_signal_time = last_signals[symbol]['time']
 
     if current_price > recommended_price:
             send_notify_signal(symbol, signal_date, "1", current_price, current_price)
-            return "Tín hiệu mua"
+            return "Mua"
     elif current_price < recommended_price:
             send_notify_signal(symbol, signal_date, "2", current_price, current_price)
-            return "Tín hiệu bán"
+            return "Bán"
 
     print("Không có tín hiệu")
     return "Không có tín hiệu"
 
 def convert_resolution_to_seconds(resolution):
     if resolution.isdigit():
-        return int(resolution) * 60  # Convert minutes to seconds
+        return int(resolution) * 60
     elif resolution == 'D':
         return 86400
     elif resolution == 'w':
-        return 604800  # 1 week in seconds
+        return 604800
     elif resolution == 'M':
-        return 2592000  # 1 month in seconds
+        return 2592000
     else:
         raise ValueError("Invalid resolution format")
